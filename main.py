69427f2a51b0b5f82bbea42d8761d90f4f16f1b5
korollzoilist <g4763862@gmail.com>
2023-05-02 10:29:01 +0500

    editing the database

--- main.py
+++ main.py
@@ -9,29 +9,30 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 logging.basicConfig(level=logging.INFO)
-AIOGRAM_API_TOKEN = os.environ.get("AIOGRAM_API_TOKEN")
+AIOGRAM_API_TOKEN = "5811386673:AAE3tD2wYOMUJr3g1jmUtLIThmeGpU6VAv0" # os.environ.get("AIOGRAM_API_TOKEN")
+admin_id = "658696815"
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 
 bot = Bot(token=AIOGRAM_API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-cur.execute("CREATE TABLE IF NOT EXISTS Students (ID INT PRIMARY KEY, username INTEGER, languages TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS Students (ID INT NOT NULL PRIMARY KEY, username TEXT NOT NULL)")
+cur.execute("CREATE TABLE IF NOT EXISTS Languages (username TEXT NOT NULL, language TEXT NOT NULL, learning_time TEXT NOT NULL, method TEXT NOT NULL)")
 
 
 class Form(StatesGroup):
-    languages = State()
+    language = State()
+    learning_time = State()
+    method = State()
 
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
+	# cur.execute(f"INSERT INTO Students (username) VALUES ()")
     await message.answer("Здравствуйте. Это бот для опроса учащихся МОБУ СОШ села Аркаулово "
                          "имени Баика Айдара насчет знания различных языков")
-    await message.answer("Напишите, пожалуйста, отдельно каждый язык, который вы когда-либо учили, срок изучения, "
-                         "уровень знания языка и метод изучения\n\n"
-                         "Например\n\nАнглийский\n3 года\nначальный уровень\nСмотрю видео на английском\n"
-                         "И отправляйте так про каждый язык\n\n"
-                         "Когда напишете все языки, отправьте \"Это всё\"")
+    await message.answer("Напишите, пожалуйста, язык, который Вы учите/учили")
 
     await Form.languages.set()
 
@@ -71,7 +72,7 @@ async def languages(message: types.Message, state: FSMContext):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.reply(message.text)
 
 
 if __name__ == '__main__':
