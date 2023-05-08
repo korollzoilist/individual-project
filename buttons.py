2923a1d649a1424c17872d3967ece1db56811734
korollzoilist <g4763862@gmail.com>
2023-05-09 02:59:48 +0500

    Creating buttons.py and developing the markups

---
+++ buttons.py
@@ -0,0 +1,8 @@
+from aiogram.types import KeyboardButton
+
+change_data_button = KeyboardButton("Изменить данные")
+add_lang_button = KeyboardButton("Добавить язык")
+finish_button = KeyboardButton("Закончить анкету")
+remove_button = KeyboardButton("Удалить язык")
+cancel_action_button = KeyboardButton("Отмена действия")
+cancel_button = KeyboardButton("Отказаться от анкеты")
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InputFile
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, InputFile
+from buttons import *
+
 
 logging.basicConfig(level=logging.INFO)
 AIOGRAM_API_TOKEN = os.environ.get("AIOGRAM_API_TOKEN")
-admin_id = "658696815"
+ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID")
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 bot = Bot(token=AIOGRAM_API_TOKEN)
@@ -21,7 +23,7 @@ cur.execute("CREATE TABLE IF NOT EXISTS Languages"
             "(user_name TEXT NOT NULL, language TEXT NOT NULL, learning_time TEXT NOT NULL, method TEXT NOT NULL)")
 
 
-class Form(StatesGroup):
+class AddLangForm(StatesGroup):
     language = State()
     learning_time = State()
     method = State()
@@ -42,29 +44,36 @@ async def start(message: types.Message):
     if message.from_user.username in [''.join(user) for user in cur.execute("SELECT username FROM Students")]:
         await message.answer("Анкета закончена")
     else:
+        cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        cancel_markup.add(cancel_button)
         await message.answer("Здравствуйте. Это бот для опроса учащихся МОБУ СОШ села Аркаулово "
-                             "имени Баика Айдара насчет знания различных языков")
-        await message.answer("Напишите, пожалуйста, язык, который Вы учите/учили")
+                             "имени Баика Айдара насчет знания различных языков\n"
+                             "Вы можете в любой момент отказаться от анкеты, нажав на кнопку \"Отказаться от анкеты\"")
+        await message.answer("Напишите, пожалуйста, язык, который Вы учите/учили", reply_markup=cancel_markup)
         cur.execute(f"INSERT INTO Students (username) VALUES (\"{message.from_user.username}\")")
-        await Form.language.set()
+        await AddLangForm.language.set()
 
 
-@dp.message_handler(state="*", commands='cancel')
-@dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
+@dp.message_handler(Text(equals="Отказаться от анкеты"), state="*")
 async def cancel(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-
     cur.execute(f"DELETE FROM Students WHERE username=\"{message.from_user.username}\"")
-
+    cur.execute(f"DELETE FROM Languages WHERE user_name=\"{message.from_user.username}\"")
+    con.commit()
     await state.finish()
-    await message.answer('Отменено')
+    await message.answer("Анкета отменена.")
 
 
-@dp.message_handler(state=Form.language)
+@dp.message_handler(Text(equals="Отмена действия"), state="*")
+async def cancel_action(message: types.Message, state: FSMContext):
+    finish_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button, cancel_button)
+    await message.answer("Действие отменено\n"
+                         "Хотите изменить данные, добавить ещё один язык, удалить язык или закончить анкету?",
+                         reply_markup=finish_markup)
+    await state.finish()
+
+
+@dp.message_handler(state=AddLangForm.language)
 async def language(message: types.Message, state: FSMContext):
     if message.text in [''.join(lang) for lang in cur.execute(
             f"SELECT language FROM Languages WHERE user_name=\"{message.from_user.username}\"").fetchall()]:
@@ -73,20 +82,20 @@ async def language(message: types.Message, state: FSMContext):
         async with state.proxy() as data:
             data['languages'] = message.text
         await message.answer("Сколько Вы учите этот язык?")
-        await Form.next()
+        await AddLangForm.next()
 
 
-@dp.message_handler(state=Form.learning_time)
+@dp.message_handler(state=AddLangForm.learning_time)
 async def learning_time(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['learning_time'] = message.text
 
     await message.answer("Напишите, как Вы учите язык (например, простмотр фильмов, сериалов, чтение книг и т.д.)")
 
-    await Form.next()
+    await AddLangForm.next()
 
 
-@dp.message_handler(state=Form.method)
+@dp.message_handler(state=AddLangForm.method)
 async def method(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['method'] = message.text
@@ -94,29 +103,25 @@ async def method(message: types.Message, state: FSMContext):
         cur.execute("INSERT INTO Languages (user_name, language, learning_time, method) VALUES"
                     f"(\"{message.from_user.username}\", \"{data['languages']}\", \"{data['learning_time']}\", "
                     f"\"{data['method']}\")")
-
-    await message.answer("Вот ваш(и) язык(и):")
+    await message.answer("Весь список добавленных Вами языков:")
     langs = cur.execute(f"SELECT * FROM Languages WHERE user_name=\"{message.from_user.username}\"").fetchall()
     for lang in langs:
         await message.answer(f"Язык: {lang[1]}\nВремя обучения: {lang[2]}\nМетоды обучения: {lang[3]}")
-    change_data_button = KeyboardButton("Изменить данные")
-    add_lang_button = KeyboardButton("Добавить язык")
-    finish_button = KeyboardButton("Закончить анкету")
-    remove_button = KeyboardButton("Удалить язык")
     finish_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button)
+    finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button, cancel_button)
     await message.answer("Данные успешно обновлены\n"
                          "Хотите изменить данные, добавить ещё один язык, удалить язык или закончить анкету?",
                          reply_markup=finish_markup)
     await state.finish()
 
 
-@dp.message_handler(lambda message: message.text == "Изменить данные")
+@dp.message_handler(Text(equals="Изменить данные"))
 async def change_data(message: types.Message):
     langs_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     langs = cur.execute(f"SELECT language FROM Languages WHERE user_name=\"{message.from_user.username}\"").fetchall()
     for lang in langs:
         langs_markup.add(KeyboardButton(lang[0]))
+    langs_markup.add(cancel_action_button, cancel_button)
     await message.answer("Какой язык Вы хотите изменить?", reply_markup=langs_markup)
     await ChangeLangForm.language.set()
 
@@ -132,20 +137,22 @@ async def change_data_2(message: types.Message, state: FSMContext):
     else:
         data_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         data_markup.add(
-            KeyboardButton("Название языка"), KeyboardButton("Время изучения"), KeyboardButton("Метод изучения"))
+            KeyboardButton("Название языка"), KeyboardButton("Время изучения"), KeyboardButton("Метод изучения"),
+            cancel_action_button, cancel_button)
         async with state.proxy() as data:
             data['language'] = message.text
         await message.answer("Какие данные Вы хотите изменить?", reply_markup=data_markup)
         await ChangeLangForm.next()
 
 
-@dp.message_handler(lambda message: message.text == "Название языка"
-                    or message.text == "Время изучения" or message.text == "Метод изучения",
+@dp.message_handler(Text(equals=["Название языка", "Время изучения", "Метод изучения"]),
                     state=ChangeLangForm.what_to_change)
 async def change_data_3(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['what_to_change'] = message.text
-    await message.answer(f"Напишите новые данные про {message.text.lower()}")
+    cancel_action_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    cancel_action_markup.add(cancel_action_button, cancel_button)
+    await message.answer(f"Напишите новые данные про {message.text.lower()}", reply_markup=cancel_action_markup)
     await ChangeLangForm.next()
 
 
@@ -164,31 +171,31 @@ async def change_data_4(message: types.Message, state: FSMContext):
     langs = cur.execute(f"SELECT * FROM Languages WHERE user_name=\"{message.from_user.username}\"").fetchall()
     for lang in langs:
         await message.answer(f"Язык: {lang[1]}\nВремя обучения: {lang[2]}\nМетоды обучения: {lang[3]}")
-    change_data_button = KeyboardButton("Изменить данные")
-    add_lang_button = KeyboardButton("Добавить язык")
-    finish_button = KeyboardButton("Закончить анкету")
-    remove_button = KeyboardButton("Удалить язык")
     finish_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button)
+    finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button, cancel_button)
     await message.answer("Данные успешно обновлены\n"
                          "Хотите изменить данные, добавить ещё один язык, удалить язык или закончить анкету?",
                          reply_markup=finish_markup)
     await state.finish()
 
 
-@dp.message_handler(lambda message: message.text == "Добавить язык")
+@dp.message_handler(Text(equals="Добавить язык"))
 async def add_another_language(message: types.Message):
-    await message.answer("Напишите, пожалуйста, язык, который Вы учите/учили")
-    await Form.language.set()
+    cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    cancel_markup.add(cancel_action_button, cancel_button)
+    await message.answer("Напишите, пожалуйста, язык, который Вы учите/учили",
+                         reply_markup=cancel_markup)
+    await AddLangForm.language.set()
 
 
-@dp.message_handler(lambda message: message.text == "Удалить язык")
+@dp.message_handler(Text(equals="Удалить язык"))
 async def delete(message: types.Message):
     langs = cur.execute(f"SELECT * FROM Languages WHERE user_name=\"{message.from_user.username}\"").fetchall()
     if langs:
         delete_langs_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         for lang in langs:
             delete_langs_markup.add(lang[1])
+        delete_langs_markup.add(cancel_action_button, cancel_button)
         await DeleteLangForm.language.set()
         await message.answer("Какой язык Вы хотите удалить из списка?", reply_markup=delete_langs_markup)
     else:
@@ -210,25 +217,23 @@ async def delete_2(message: types.Message, state: FSMContext):
                 await message.answer(f"Язык: {lang[1]}\nВремя обучения: {lang[2]}\nМетоды обучения: {lang[3]}")
         else:
             await message.answer("Языков нет")
-        change_data_button = KeyboardButton("Изменить данные")
-        add_lang_button = KeyboardButton("Добавить язык")
-        finish_button = KeyboardButton("Закончить анкету")
-        remove_button = KeyboardButton("Удалить язык")
         finish_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button)
+        finish_markup.add(change_data_button, add_lang_button, finish_button, remove_button, cancel_button)
         await message.answer("Данные успешно обновлены\n"
                              "Хотите изменить данные, добавить ещё один язык, удалить язык или закончить анкету?",
                              reply_markup=finish_markup)
         await state.finish()
 
 
-@dp.message_handler(lambda message: message.text == "Закончить анкету")
+@dp.message_handler(Text(equals="Закончить анкету"))
 async def finish(message: types.Message):
-    await message.answer("Спасибо за участие в анкете!")
+    await message.answer("Спасибо за участие в анкете!", reply_markup=ReplyKeyboardRemove())
     con.commit()
-    await bot.send_message(chat_id=658696815, text=f"{message.from_user.username} заполнил анкету:")
-    await bot.send_document(chat_id=658696815, document=InputFile("database.db"))
+    people_count = len(cur.execute("SELECT ID FROM Students").fetchall())
+    await bot.send_message(chat_id=ADMIN_CHAT_ID, text=f"{message.from_user.username} заполнил(а) анкету:"
+                                                       f"\nОбщее число заполнивших анкету: {people_count}")
+    await bot.send_document(chat_id=ADMIN_CHAT_ID, document=InputFile("database.db"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
